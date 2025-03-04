634cc7c7a0bccb15a99df74cf38ff1aee03c23e5
roshanvrazak <roshanvrazak@gmail.com>
2025-03-04 13:14:12 +0000

    Initial commit

---
+++ src/main/java/com/rvr/springboot/bean/UserConfig.java
@@ -0,0 +1,27 @@
+package com.rvr.springboot.bean;
+
+public class UserConfig {
+    private String name;
+    private String className;
+
+    public UserConfig(String name, String className) {
+        this.name = name;
+        this.className = className;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
