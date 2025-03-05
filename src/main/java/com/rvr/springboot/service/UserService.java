3555dd75ac9fb16517d7672f0d07d72d5810ec38
roshanvrazak <roshanvrazak@gmail.com>
2025-03-05 20:28:06 +0000

    package name changes

---
+++ src/main/java/com/rvr/springboot/service/UserService.java
@@ -0,0 +1,13 @@
+package com.rvr.springboot.service;
+
+import com.rvr.springboot.bean.UserConfig;
+
+public class UserService {
+
+    UserConfig userConfig;
+
+    public void setUserConfig(UserConfig userConfig) {
+        this.userConfig = userConfig;
+    }
+
+}
